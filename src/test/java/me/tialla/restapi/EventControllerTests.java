b3d27e07610e2a03292e38856e835bc10a4f22ca
tialla-kka <tialla99@hanmail.net>
2020-12-07 20:32:10 +0900

    DTO @Vailid hibernate-validator // wrong data

--- src/test/java/me/tialla/restapi/EventControllerTests.java
+++ src/test/java/me/tialla/restapi/EventControllerTests.java
@@ -110,4 +110,27 @@ public class EventControllerTests {
         ;
     }
 
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("Rest API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,12,10,15,6))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,12,8,11,6))
+                .beginEventDateTime(LocalDateTime.of(2020,12,7,15,1))
+                .endEventDateTime(LocalDateTime.of(2020,12,7,16,1))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("감남역 D2 스타일 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest())
+
+        ;
+    }
+
 }
