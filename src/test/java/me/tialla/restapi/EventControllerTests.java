de6aacb2490e30f6c6baf7a3e6b437eace55b739
tialla-kka <tialla99@hanmail.net>
2020-12-07 20:42:22 +0900

    Add test description annotation

--- src/test/java/me/tialla/restapi/EventControllerTests.java
+++ src/test/java/me/tialla/restapi/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.tialla.restapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.tialla.restapi.common.TestDescription;
 import me.tialla.restapi.events.Event;
 import me.tialla.restapi.events.EventDto;
 import me.tialla.restapi.events.EventStatus;
@@ -34,6 +35,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception{
 
         EventDto event = EventDto.builder()
@@ -67,6 +69,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception{
 
         Event event = Event.builder()
@@ -99,6 +102,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -111,6 +115,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
