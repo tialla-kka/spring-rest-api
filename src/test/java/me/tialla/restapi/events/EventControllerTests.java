77f1120be54347eeb240ed46f009e029b1424433
tialla-kka <tialla99@hanmail.net>
2020-12-10 16:51:46 +0900

    BaseControllerTest (Test code Refactoring)

--- src/test/java/me/tialla/restapi/events/EventControllerTests.java
+++ src/test/java/me/tialla/restapi/events/EventControllerTests.java
@@ -1,26 +1,12 @@
 package me.tialla.restapi.events;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import me.tialla.restapi.common.RestDocsConfiguration;
+import me.tialla.restapi.common.BaseControllerTest;
 import me.tialla.restapi.common.TestDescription;
-import me.tialla.restapi.events.Event;
-import me.tialla.restapi.events.EventDto;
-import me.tialla.restapi.events.EventStatus;
-import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
@@ -34,25 +20,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTests {
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
+public class EventControllerTests extends BaseControllerTest {
 
     @Autowired
     EventRepository eventRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
 
     @Test
     @TestDescription("정상적으로 이벤트를 생성하는 테스트")
