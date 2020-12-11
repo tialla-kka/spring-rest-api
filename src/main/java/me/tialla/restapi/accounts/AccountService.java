10e60c80f2d5047155987840973a14f9838280ca
tialla-kka <tialla99@hanmail.net>
2020-12-11 09:57:17 +0900

    Add Spring Security

---
+++ src/main/java/me/tialla/restapi/accounts/AccountService.java
@@ -0,0 +1,31 @@
+package me.tialla.restapi.accounts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email).orElseThrow(() ->new UsernameNotFoundException(email));
+        return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream().map(r->new SimpleGrantedAuthority("ROLE_"+r.name())).collect(Collectors.toSet());
+    }
+}
---
+++ src/test/java/me/tialla/restapi/accounts/AccountServiceTest.java
@@ -0,0 +1,47 @@
+package me.tialla.restapi.accounts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername(){
+        //Given
+        String password ="pass";
+        String email = "tialla@email.com";
+        Account account = Account.builder()
+                .email(email)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        this.accountRepository.save(account);
+
+        //When
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        //Then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+}
